94c28cf630217e2904d07701c6efcfd215ba8271
Ryan Soklaski <ry26099@mit.edu>
2022-04-20 18:19:55 -0400

    remove redundant optimizers

--- src/rai_toolbox/optim/__init__.py
+++ src/rai_toolbox/optim/__init__.py
@@ -2,10 +2,6 @@
 # Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
 # SPDX-License-Identifier: MIT
 
-import functools as _fn
-
-from torch.optim import SGD as _SGD
-
 from .frank_wolfe import (
     FrankWolfe,
     L1FrankWolfe,
@@ -15,26 +11,13 @@ from .frank_wolfe import (
 )
 from .lp_space import (
     L1NormedGradientOptim,
+    L1qNormedGradientOptim,
     L2NormedGradientOptim,
-    SignedGradientOptim,
     L2ProjectedOptim,
     LinfProjectedOptim,
-    L1qNormedGradientOptim,
-    L2NormedGradientOptim,
+    SignedGradientOptim,
 )
 from .optimizer import GradientTransformerOptimizer, ProjectionMixin
-
-# TODO: Get rid of L2PGD and LinfPGD -- their default optimizers are already SGD
-L2PGD = _fn.update_wrapper(
-    _fn.partial(L2ProjectedOptim, InnerOpt=_SGD), L2ProjectedOptim
-)
-LinfPGD = _fn.update_wrapper(
-    _fn.partial(LinfProjectedOptim, InnerOpt=_SGD), LinfProjectedOptim
-)
-L1qFW = _fn.update_wrapper(
-    _fn.partial(L1qNormedGradientOptim, InnerOpt=FrankWolfe),
-    L1qNormedGradientOptim,
-)
 
 __all__ = [
     "L1FrankWolfe",
@@ -50,7 +33,4 @@ __all__ = [
     "LinfProjectedOptim",
     "GradientTransformerOptimizer",
     "ProjectionMixin",
-    "L2PGD",
-    "LinfPGD",
-    "L1qFW",
 ]
