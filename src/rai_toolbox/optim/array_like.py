433c741e8629a51fda2c3bff8fcf5a72594685da
Ryan Soklaski <ry26099@mit.edu>
2022-04-28 02:01:55 -0400

    add header

--- src/rai_toolbox/optim/array_like.py
+++ src/rai_toolbox/optim/array_like.py
@@ -1,3 +1,7 @@
+# Copyright 2022, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
 from typing import Any, Dict, Optional, Union
 
 import numpy as np
