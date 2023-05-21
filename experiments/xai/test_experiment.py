ccbbda9c3f1c0e543a76b56ada6a2a64b1377aea
Justin Goodwin <jgoodwin@ll.mit.edu>
2023-05-21 13:29:01 -0400

    consolidate viz experiment; remove hydra-zen

--- experiments/xai/test_experiment.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
-# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
-# SPDX-License-Identifier: MIT
-
-
-def test_configs():
-    from configs import L1FW
-
-    assert L1FW  # prevent removal of unused imports
